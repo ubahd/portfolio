b6ac8994cb7ad590f6a864c05f9c697d94aeded9
David <ubah.davidt@gmail.com>
2020-08-12 16:32:49 -0400

    Updates

---
+++ precache-manifest.f9711d3588bdce97d4b494075fdad2a4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3387e23d1641dfd8870d7cb765a6645a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bea614ee6c5868c8d2dd",
+    "url": "/portfolio/static/css/main.1e87797f.chunk.css"
+  },
+  {
+    "revision": "5f87344cbc59a43d7d99",
+    "url": "/portfolio/static/js/2.d97ebc81.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/portfolio/static/js/2.d97ebc81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea614ee6c5868c8d2dd",
+    "url": "/portfolio/static/js/main.2857555a.chunk.js"
+  },
+  {
+    "revision": "50e8ed6d4a2c8300265d",
+    "url": "/portfolio/static/js/runtime-main.13735310.js"
+  },
+  {
+    "revision": "dda222674484d4c128d1d0bfb85421b2",
+    "url": "/portfolio/static/media/adobe.dda22267.png"
+  },
+  {
+    "revision": "e7c9f4b808ecbcdb8365ab2d50628155",
+    "url": "/portfolio/static/media/aws.e7c9f4b8.png"
+  },
+  {
+    "revision": "19cf08304faf9c8c35df687e655f894b",
+    "url": "/portfolio/static/media/dbeaver.19cf0830.png"
+  },
+  {
+    "revision": "1410fa8d0fdaaa8bec332e6fefdc632a",
+    "url": "/portfolio/static/media/postgresql.1410fa8d.png"
+  },
+  {
+    "revision": "88b91137c36e137332e520cef664935e",
+    "url": "/portfolio/static/media/postman.88b91137.png"
+  },
+  {
+    "revision": "2ce79a41609fdbab11326b93c30d5284",
+    "url": "/portfolio/static/media/profile.2ce79a41.jpg"
+  },
+  {
+    "revision": "292ea018923198e5b74cb61cb2e051f5",
+    "url": "/portfolio/static/media/react.292ea018.png"
+  },
+  {
+    "revision": "c24be32d09a6251bf262f463e5db9975",
+    "url": "/portfolio/static/media/sass.c24be32d.png"
+  },
+  {
+    "revision": "502d09aead02c19b6b16691731e30ec8",
+    "url": "/portfolio/static/media/storybook.502d09ae.png"
+  }
+]);
